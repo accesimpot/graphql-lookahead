0c588a729b1d32328e482376740afa9e55e86c9b
Pierre-Michel Brown <pierremichelbrown@gmail.com>
2024-08-08 21:29:29 +0200

    Add first lookahead implementation

--- packages/playground/src/graphql-yoga/resolvers.ts
+++ packages/playground/src/graphql-yoga/resolvers.ts
@@ -1,12 +1,48 @@
 import type { createSchema } from 'graphql-yoga'
+import { lookahead } from 'graphql-lookahead'
 import { mockFullCart } from '../mockData'
 
 type Resolver = NonNullable<Parameters<typeof createSchema>[0]['resolvers']>
 
+interface QueryFilter {
+  model?: string
+  include?: (QueryFilter | string)[]
+}
+
 export const resolvers: Resolver = {
   Query: {
-    order: (_parent, _args, _context, _info) => {
-      // console.log(info.returnType)
+    order: (_parent, _args, _context, info) => {
+      const queryFilters: QueryFilter = {}
+
+      lookahead({
+        info,
+        state: queryFilters,
+
+        next({ parentState, typeName }) {
+          const nextState: QueryFilter = { model: typeName }
+
+          parentState.include = parentState.include || []
+          parentState.include.push(nextState)
+
+          return nextState
+        },
+      })
+
+      console.dir({ queryFilters }, { depth: null })
+
+      // => {
+      //   queryFilters: {
+      //     include: [
+      //       {
+      //         model: 'OrderItem',
+      //         include: [
+      //           { model: 'Product', include: [ { model: 'Inventory' } ] },
+      //           { model: 'ProductGroup' }
+      //         ]
+      //       }
+      //     ]
+      //   }
+      // }
 
       return mockFullCart
     },
