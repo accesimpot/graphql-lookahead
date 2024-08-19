88207527f9b00c930a9ca4dc5c463737f739b333
Pierre-Michel Brown <pierremichelbrown@gmail.com>
2024-08-19 16:24:51 +0200

    Add thresholds to vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,13 @@ export default defineConfig({
       enabled: true,
       provider: 'istanbul',
       include: ['packages/graphql-lookahead/src/**'],
+
+      thresholds: {
+        statements: 94,
+        branches: 84,
+        functions: 100,
+        lines: 100,
+      },
     },
   },
 })
